c6f31e07ce6421d97e0b1652b57405b76378c106
SallyNguyen2002 <anh.sally1508@gmail.com>
2025-04-03 14:54:50 -0600

    Update home.js
    add js for choose number of guests booking

--- home.js
+++ home.js
@@ -22,3 +22,27 @@ window.onload = function () {
       });
     }
 
+    const guestInput = document.getElementById("guests");
+    const guestIcon = document.getElementById("openGuest");
+    const guestDropdown = document.getElementById("guestDropdown");
+
+    guestIcon.addEventListener("click", () => {
+        guestDropdown.classList.toggle("hidden");
+    });
+
+    guestDropdown.querySelectorAll("li").forEach(item => {
+        item.addEventListener("click", () => {
+        guestInput.value = item.dataset.value;
+        guestDropdown.classList.add("hidden");
+        });
+    });
+
+    // Optional: close dropdown if you click outside
+    document.addEventListener("click", (e) => {
+        if (!guestIcon.contains(e.target) && !guestDropdown.contains(e.target)) {
+        guestDropdown.classList.add("hidden");
+        }
+    });
+
+  };
+  
